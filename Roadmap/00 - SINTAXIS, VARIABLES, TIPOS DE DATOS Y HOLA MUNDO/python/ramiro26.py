1691ffc57f574e6d1614b0f078274b6ac08c726b
posan26 <ramiroignacio.posanzini@gmail.com>
2024-09-06 15:30:41 -0300

    Create ramiro26.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/ramiro26.py
@@ -0,0 +1,34 @@
+#https://www.python.org         
+
+# Comentarios Simples   
+
+"""
+Comentarios de
+lineas
+Multiples
+""" 
+#String
+cadena = "Ramiro"
+
+#Variable
+Variable =10 
+
+#Constante (no existen asi que se escriben en Mayusculas para que se sepa que njo se debe modificar)
+MI_CONSTANTE =11
+
+
+ #NUMERICOS 
+numero_entero = 2
+numero_float = 2.1   
+numero_complejos = 3 + 4j
+
+#Booleanos 
+Mi_nombre_es_ramiro = True
+Mi_nombre_no_es_ramiro = False
+
+lista = [1,2,3]
+
+None = None
+
+print("hola, phyton")
+
